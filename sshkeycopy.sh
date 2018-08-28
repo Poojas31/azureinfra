bdec33f7cb2403ce288d2dc4396ceabd08feeb34
kumarranja <yadavranjan33@gmail.com>
2018-08-28 10:59:35 +0000

    latest

--- sshkeycopy.sh
+++ sshkeycopy.sh
@@ -1,2 +1,2 @@
 chmod +x /home/mychatops/mycode/sshconnectivity.sh
-sudo /home/mychatops/mycode/sshconnectivity.sh "welcome#1234" "104.43.241.192" "ranjanku"
+sudo /home/mychatops/mycode/sshconnectivity.sh "welcome#1234" "104.43.253.212" "ranjanku"
